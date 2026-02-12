4573eb60b548c4ca05243e9513179f797e132e69
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2026-02-12 22:04:03 +0530

    Wow. I still can't get this right. Wow.

--- 4/StrReverse.c
+++ 4/StrReverse.c
@@ -1,9 +1,31 @@
+#include "Util.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main(int const p_argCount, char *const p_argValues[]) {
+char* strrev(char *const p_string, size_t const p_length) {
+	for (size_t i = 0; i < p_length / 2; i++) {
 
-	
+		char const c2 = p_string[p_length - i - 1];
+		char const c1 = p_string[i];
 
-	exit(EXIT_SUCCESS);
+		p_string[p_length - i - 1] = c1;
+		p_string[i] = c2;
+
+	}
+
+	return p_string;
+}
+
+int main(int const p_argCount, char const *const p_argValues[]) {
+	// exit(EXIT_SUCCESS);
+	char *str = "reverted";
+	size_t const len = strlen(str);
+
+	char *buf = calloc(len, sizeof(str[0]));
+	memcpy(buf, str, len);
+
+	buf = strrev(buf, len);
+	printf("Reverted string: \"%s\".\n", buf);
+	free(buf);
 }
