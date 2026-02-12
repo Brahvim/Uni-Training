045f0368b20fc3670638f0ddbdac0b08a0ed6feb
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2026-02-12 15:00:11 +0530

    Something-something buildsys stuff... (from last class!).

--- Semester-3/Dec2Bin.c
+++ Semester-3/Dec2Bin.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
+
 int main(int argc, char const *argv[]) {
 
-    int twoButNu
-    
-    m = 1;
+    int twoButNum = 1;
     printf("%d%d",
         twoButNum & 1 ? "0" : "1",
         twoButNum & 2 ? "0" : "1"
---
+++ Semester-4/StrReverse.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int const p_argCount, char *const p_argValues[]) {
+
+	
+
+	exit(EXIT_SUCCESS);
+}
