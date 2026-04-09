59f820e924a967e7963d78cf8a61a567ab40ef88
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2026-04-09 15:24:08 +0530

    ...Ehhh,

---
+++ src/4/ArraySubsetsRecursive.c
@@ -0,0 +1,85 @@
+#include "Util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+// E.g.
+// [
+// 	[ ]
+// 	[ 5 ]
+// 	[ 5, 6 ]
+// 	[ 5, 6, 7 ]
+// 	[ 5, 7 ]
+// 	[ 6, 7 ]
+// 	[ 7 ]
+// ], for [ 5, 6, 7 ].
+// These can also be strings e.g. `"abc"`.
+
+int* attempt1(int const *const p_array, int const p_length) {
+	for (size_t i = 0; i < p_length; i++) {
+
+		puts("[ ");
+
+		for (size_t j = i + 1; j < p_length; j++) {
+
+			if (j == i) {
+
+				break;
+
+			}
+			else if (j > p_length) {
+
+				j = 0;
+
+			}
+			else {
+
+				printf("%d ", p_array[j]);
+
+			}
+
+		}
+
+		puts(" ]");
+
+	}
+}
+
+int* solutionFromTeacher(int const *const p_array, int const p_length) {
+	for (size_t i = 0; i < p_length; i++) {
+
+		puts("[ ");
+
+		for (size_t j = i; j < p_length; j++) {
+
+			for (size_t k = 0; k < i + 1; k++) {
+
+				if (j == i) {
+
+					break;
+
+				}
+				else if (j > p_length) {
+
+					j = 0;
+
+				}
+				else {
+
+					printf("%d ", p_array[j]);
+
+				}
+
+			}
+
+			puts(" ]");
+
+		}
+	}
+}
+
+int main(int const p_argc, char const *const *p_argv) {
+	int const arr[] = { 5, 6, 7 };
+	int const len = sizearr(arr);
+	attempt1(arr, len);
+}
